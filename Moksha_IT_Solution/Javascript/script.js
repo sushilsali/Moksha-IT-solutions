6bf43239051419d996392cde521755792bc05d71
sushilsali <sushilsali@gmail.com>
2016-11-30 08:46:35 +0530

    Added by Diksha Jain for MapController Changes.

--- Moksha_IT_Solution/Javascript/script.js
+++ Moksha_IT_Solution/Javascript/script.js
@@ -11,6 +11,7 @@ $routeProvider.when("/home",{
 }).when("/contact",{
  
    templateUrl:"Templates/contact.html",
+   controller:"mapController"
 
 
 }).otherwise({
@@ -22,4 +23,32 @@ $routeProvider.when("/home",{
 
 })
 
-app.controller("homeController",function($scope){})
+app.controller("homeController",function($scope){
+
+	$scope.test=function(){
+
+  angular.element('#about').triggerHandler('click');
+	};
+})
+
+app.controller("mapController",function($scope){
+
+var myCenter = new google.maps.LatLng(41.878114, -87.629798);
+
+ var mapProp = {
+  center:myCenter,
+  zoom:12,
+  scrollwheel:false,
+  draggable:false,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+
+  $scope.map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+ $scope.marker = new google.maps.Marker({
+  position:myCenter
+  });
+
+$scope.marker.setMap($scope.map);
+})
